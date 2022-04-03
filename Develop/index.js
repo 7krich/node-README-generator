8cad4dbdccbb77bc43ff450c57f0762522682999
Kyle Richnafsky <7krich@gmail.com>
2022-04-03 18:28:42 -0400

    questions added

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,108 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// TODO: Create an object containing questions for user input
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the name of your project!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project. (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'instructions',
+            message: 'Please list the installation instructions.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the name of your project!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'guidelines',
+            message: 'List the contribution guidelines.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the name of your project!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Add the test instructions.',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the test instructions!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'checkbox',
+            name: 'licenses',
+            message: 'What licenses need to be added to the README? (Check all that apply)',
+            choices: ['MIT', 'MSU', 'MDN', 'W3', 'Apple', 'Google', 'Microsoft']
+          },
+          {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub Username',
+            validate: nameInput => {
+              if (nameInput) {
+                return true;
+              } else {
+                console.log('Please enter your GitHub username!');
+                return false;
+              }
+            }
+          },
+          {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your GitHub e-mail',
+            validate: nameInput => {
+              if (nameInput) {
+                return true;
+              } else {
+                console.log('Please enter your e-mail!');
+                return false;
+              }
+            }
+          },
+    ])
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
