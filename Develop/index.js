6032c637d4770ab83946e10350abd6ac789eb43f
Kyle Richnafsky <7krich@gmail.com>
2022-04-03 19:20:16 -0400

    allowed for responses & updated questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,9 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const generatePage = require('./utils/generateMarkdown');
 
 // TODO: Create an object containing questions for user input
-const questions = () => {
+const promptUser = () => {
     return inquirer.prompt([
         {
             type: 'input',
@@ -59,7 +60,7 @@ const questions = () => {
         {
             type: 'input',
             name: 'test',
-            message: 'Add the test instructions.',
+            message: 'What are the test intrucitons?',
             validate: nameInput => {
                 if (nameInput) {
                     return true;
@@ -91,7 +92,7 @@ const questions = () => {
           {
             type: 'input',
             name: 'email',
-            message: 'Enter your GitHub e-mail',
+            message: 'Enter your e-mail',
             validate: nameInput => {
               if (nameInput) {
                 return true;
@@ -102,13 +103,29 @@ const questions = () => {
             }
           },
     ])
+
 };
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// const writeFile = fileContent => {
+//     return new Promise((resolve, reject) => {
+//         fs.writeFile('./index.html', fileContent, err => {
+//             if (err) {
+//                 reject (err);
+//                 return;
+//             }
+
+//             resolve ({
+//                 ok: true,
+//                 message: 'File created!'
+//             });
+//         });
+//     });
+// };
 
 // TODO: Create a function to initialize app
-function init() {}
+//function init() {}
 
 // Function call to initialize app
-init();
+promptUser()
+    .then(answers => console.log(answers));
